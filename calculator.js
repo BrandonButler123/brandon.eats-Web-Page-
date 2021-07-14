1347c5dd23b7b14c5eea73403800e7c695aa4d83
BrandonButler123 <85582593+BrandonButler123@users.noreply.github.com>
2021-07-14 12:01:26 -0230

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+
+
+function volumeConverter(valNum) {
+    document.getElementById("outputMilliliters").innerHTML = 14.7868*valNum;
+}
+
+function volumeConverter1(valNum) {
+    document.getElementById("outputMilliliters1").innerHTML = 4.92892*valNum;
+}
+function volumeConverter2(valNum) {
+    document.getElementById("outputMilliliters2").innerHTML = 1000*valNum;
+}
+
+function volumeConverter3(valNum) {
+    document.getElementById("outputMilliliters3").innerHTML = 29.5735*valNum;
+}
